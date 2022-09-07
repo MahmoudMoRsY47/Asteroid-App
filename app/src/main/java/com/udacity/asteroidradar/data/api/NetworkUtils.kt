4be78c195085c92ad091c5621cb88263db0483db
MahmoudMoRsY47 <MahmoudMoRsY47>
2022-09-07 04:51:17 +0200

    commit

--- app/src/main/java/com/udacity/asteroidradar/data/api/NetworkUtils.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/api/NetworkUtils.kt
@@ -63,11 +63,6 @@ fun getToday(): String {
     return formatDate(calendar.time)
 }
 
-fun getSeventhDay(): String {
-    val calendar = Calendar.getInstance()
-    calendar.add(Calendar.DAY_OF_YEAR, 7)
-    return formatDate(calendar.time)
-}
 private fun formatDate(date: Date): String {
     val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
     return dateFormat.format(date)
